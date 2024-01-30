ed2d7f5ede549b672cf88cbb8a989f0d3eef69e4
AZZAHRAH <110447871+azzahrahr@users.noreply.github.com>
2024-01-30 23:47:12 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,23 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+import option_menu
 import webbrowser
-import sys
-
-try:
-    import streamlit as st
-    from streamlit_option_menu import option_menu
-except ImportError:
-    print("Some required packages are not installed, installing...")
-    sys.exit(
-        subprocess.check_call(
-            [
-                "pip",
-                "install",
-                "streamlit",
-                "streamlit-option-menu",
-            ]
-        )
-    )
 
 st.title("Nada Shop")
 st.image("images/ayam.jpg")
