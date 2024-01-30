cbd7023293b35c62a30a7899c52b02400d737a94
AZZAHRAH <110447871+azzahrahr@users.noreply.github.com>
2024-01-31 00:09:37 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sys
 
-sys.path.insert(1, "C:\Users\ACER\streamlit\nada\stenv\Lib\site-packages\streamlit_option_menu")
+sys.path.insert(1, "C:/Users/ACER/streamlit/nada/stenv/Lib/site-packages/streamlit_option_menu")
 
 from streamlit_option_menu import option_menu
 
