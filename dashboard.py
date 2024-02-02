a654095f286698ba5bd278e75364c212a5b87f56
AZZAHRAH <110447871+azzahrahr@users.noreply.github.com>
2024-02-02 13:19:30 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ with tab1:
     col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.subheader("Dimsum Ayam")
-        st.subheader("Rp. 50.000")
+        st.subheader(:red[Rp. 50.000])
         st.image("images/ayam.jpg")
         st.write("----------------")
         st.subheader("Lumpia")
